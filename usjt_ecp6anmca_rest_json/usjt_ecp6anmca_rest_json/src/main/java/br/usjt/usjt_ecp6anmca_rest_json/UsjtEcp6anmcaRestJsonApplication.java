20669a6e5720bb2ed0970d9ff71ede67075ebedd
Nicolas1606 <61884793+Nicolas1606@users.noreply.github.com>
2020-06-07 19:33:54 -0300

    Add files via upload
    Aula 12 Json

---
+++ usjt_ecp6anmca_rest_json/usjt_ecp6anmca_rest_json/src/main/java/br/usjt/usjt_ecp6anmca_rest_json/UsjtEcp6anmcaRestJsonApplication.java
@@ -0,0 +1,14 @@
+package br.usjt.usjt_ecp6anmca_rest_json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class UsjtEcp6anmcaRestJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsjtEcp6anmcaRestJsonApplication.class, args);
+	}
+
+}
---
+++ usjt_ecp6anmca_rest_json/usjt_ecp6anmca_rest_json/src/test/java/br/usjt/usjt_ecp6anmca_rest_json/UsjtEcp6anmcaRestJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.usjt.usjt_ecp6anmca_rest_json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsjtEcp6anmcaRestJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
